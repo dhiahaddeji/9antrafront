e420aeb18e0ea85f9adc3c0bc4415f5b18196978
dhia <dhia.haddeji@esprit.tn>
2026-07-20 10:15:35 +0000

    fix: add RouterModule, use CSS class toggle for dropdown instead of *ngIf

--- src/app/views/admin/admin-trainings/admin-trainings.module.ts
+++ src/app/views/admin/admin-trainings/admin-trainings.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { AdminTrainingsRoutingModule } from './admin-trainings-routing.module';
 import { AdminTrainingsComponent } from './admin-trainings/admin-trainings.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AdminTrainingsComponent
   ],
   imports: [
-    
     CommonModule,
+    RouterModule,
     AdminTrainingsRoutingModule,
     FormsModule,
     ReactiveFormsModule
